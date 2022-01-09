691141523ad9f29426e6b5048a27abf239da37e9
Denys Shabalin <shabalin@google.com>
2022-01-09 13:41:03 +0100

    Fix scala permute being different from other languages

--- Benchmarking/scala/src/main/scala/PermuteBenchmark.scala
+++ Benchmarking/scala/src/main/scala/PermuteBenchmark.scala
@@ -1,24 +1,24 @@
 package permute
 
 object PermuteBenchmark {
-  var count = 0
+  class Counter { var count = 0 }
 
   def run(n: Int): Int = {
     val arr = new Array[Int](n)
-    count = 0
-    permute(arr, n)
-    return count
+    val counter = new Counter
+    permute(counter, arr, n)
+    return counter.count
   }
 
-  def permute(arr: Array[Int], n: Int): Unit = {
-    count += 1
+  def permute(counter: Counter, arr: Array[Int], n: Int): Unit = {
+    counter.count += 1
     if (n != 0) {
       val n1 = n - 1
-      permute(arr, n1)
+      permute(counter, arr, n1)
       var i = n1
       while (i >= 0) {
         swap(arr, n1, i)
-        permute(arr, n1)
+        permute(counter, arr, n1)
         swap(arr, n1, i)
         i -= 1
       }
